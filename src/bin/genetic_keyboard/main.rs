e60e848ffc3c636d7d879463cb85a8ec23261d2f
s. galiamov <sgalyamov@gmail.com>
2021-01-15 21:21:03 +0100

    commented a code

--- src/bin/genetic_keyboard/main.rs
+++ src/bin/genetic_keyboard/main.rs
@@ -1,9 +1,9 @@
-mod calculation;
+// mod calculation;
 
-use calculation::run;
-use ed_balance::models::CliSettings;
-use std::process;
-use structopt::StructOpt;
+// use calculation::run;
+// use ed_balance::models::CliSettings;
+// use std::process;
+// use structopt::StructOpt;
 
 // 0. load texts in memory
 // 1. create initial population
@@ -14,9 +14,9 @@ use structopt::StructOpt;
 
 
 fn main() {
-    let args = CliSettings::from_args();
-    if let Err(e) = run(args) {
-        eprintln!("Calculations failed: {:#?}", e);
-        process::exit(1);
-    }
+    // let args = CliSettings::from_args();
+    // if let Err(e) = run(args) {
+    //     eprintln!("Calculations failed: {:#?}", e);
+    //     process::exit(1);
+    // }
 }
