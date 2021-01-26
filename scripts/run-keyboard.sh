5589325e3f20b83df8be8f2a87c40c7bc4f4c137
s. galiamov <sgalyamov@gmail.com>
2021-01-26 23:35:01 +0100

    almost

--- scripts/run-keyboard.sh
+++ scripts/run-keyboard.sh
@@ -2,9 +2,9 @@ clear
 
 ./target/publish/genetic_keyboard.exe \
     -k ./data/keyboard.json \
-    -p 1000 \
-    -c 3 \
+    -p 500 \
+    -c 2 \
     -g 10000 \
-    -m 3 \
+    -m 2 \
     --results-count 20 \
     --repeats-count 100
