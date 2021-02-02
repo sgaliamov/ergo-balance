7fc18de06ff187833bcc6d0d7d044c9610e6cb00
s. galiamov <sgalyamov@gmail.com>
2021-02-02 11:35:44 +0100

    11520

--- scripts/keyboard.sh
+++ scripts/keyboard.sh
@@ -3,11 +3,11 @@ cargo +nightly build --release --out-dir=./target/publish -Z unstable-options
 clear
 
 ./target/publish/genetic_keyboard.exe \
-    -t ./data/samples/0.05-367.result.txt \
+    -t ./data/samples/0.05-11520.result.txt \
     -k ./data/keyboard.json \
-    -p 500 \
-    -c 5 \
+    -p 600 \
+    -c 4 \
     -g 10000 \
     -m 5 \
-    --results-count 35 \
-    --repeats-count 150
+    --results-count 30 \
+    --repeats-count 100
